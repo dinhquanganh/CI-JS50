0b925374a38fc22c84a274a25aa2d3e76e7c84b3
Đinh Quang Anh <56481830+dinhquanganh@users.noreply.github.com>
2020-09-29 03:23:56 +0700

    update

---
+++ lession4/b3/script/app.js
@@ -0,0 +1,20 @@
+class MyFormSign extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+    let $template = document.getElementById("form-sign");
+    this.shadowRoot.appendChild($template.content.cloneNode(true));
+
+    this.$username = this.shadowRoot.getElementById("username");
+    this.$email = this.shadowRoot.getElementById("email");
+    this.$password = this.shadowRoot.getElementById("password");
+    this.$re_pasword = this.shadowRoot.getElementById("re_pasword");
+  }
+
+  static get observedAttributes() {
+    return ["username", "email", "password", "re_pasword"];
+  }
+
+
+}
+window.customElements.define("form-sign", MyFormSign);
